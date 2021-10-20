a33416374fa2f813d2e65764543eba580a9a66b1
mvn_rvn <xismiranims@hotmail.com>
2021-10-20 18:32:56 -0400

    Fixed gravity

--- Assets/Scripts/TestPlayerScript.cs
+++ Assets/Scripts/TestPlayerScript.cs
@@ -8,6 +8,10 @@ public class TestPlayerScript : MonoBehaviour
     Vector2 movement;
     bool jump;
     float velocity = 0f;
+    //public vars
+    public float gravity_mod = 1f;
+    public float jump_height = 2f;
+    public float speed_mod = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,31 +34,30 @@ public class TestPlayerScript : MonoBehaviour
         }
         //jump
         if(Input.GetButton("Jump")) {
-            RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+            RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down, 1.02f, LayerMask.GetMask("Ground"));
             if(ground.collider != null) {
                 jump = true;
                 Debug.Log("JUMP YOU HECKER JUMP");
-            } else {
-                jump = false;
             }
-        } else {
-            jump = false;
         }
     }
 
     void FixedUpdate() {
-        //movement
-        rb.MovePosition((Vector2)transform.position + (movement * Time.deltaTime * 10));
         //gravity
-        RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+        RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down, 1.02f, LayerMask.GetMask("Ground"));
         if(ground.collider == null) {
-            velocity -= 1 * Time.deltaTime;
-            rb.MovePosition((Vector2)transform.position + new Vector2(0, velocity));
+            Debug.Log("On ground");
+            velocity -= gravity_mod * Time.deltaTime;
         } else {
-            velocity = 0;
+            velocity = 0f;
         }
+        //jumping
         if(jump == true) {
-            velocity = 20;
+            velocity = 2f;
+            jump = false;
         }
+        //movement
+        movement.y += velocity;
+        rb.MovePosition((Vector2)transform.position + (movement * Time.deltaTime * 10));
     }
 }
