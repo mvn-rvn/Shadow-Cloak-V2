08386fd755da5c6316db71a192b382e1ce8359fb
mvn_rvn <xismiranims@hotmail.com>
2021-11-11 17:45:20 -0500

    Dash jumping and code readability

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //normal/private vars
     Rigidbody2D rb;
     SpriteRenderer sprite;
     Vector2 movement = new Vector2(0, 0);
@@ -12,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     bool double_jump = false;
     float original_speed_mod;
     float original_gravity_mod;
+    
     //public vars
     public float gravity_mod = 1f;
     public float jump_height = 2f;
@@ -19,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     public float dash_multiplier = 2f;
     public float fastfall_multiplier = 1.5f;
     public float fall_speed_limit = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,30 +40,60 @@ public class PlayerMovement : MonoBehaviour
         //transform.up = direction;
         
         //ground detection
-        RaycastHit2D ground = Physics2D.CircleCast(transform.position, 0.4f, Vector2.down, 0.62f, LayerMask.GetMask("Ground"));
-        //wall detection
-        RaycastHit2D left_wall = Physics2D.BoxCast(transform.position, new Vector2(0.5f, 1.9f), 0f, Vector2.left, 0.27f, LayerMask.GetMask("Ground"));
-        RaycastHit2D right_wall = Physics2D.BoxCast(transform.position, new Vector2(0.5f, 1.9f), 0f, Vector2.right, 0.27f, LayerMask.GetMask("Ground"));
+        RaycastHit2D ground = Physics2D.CircleCast(
+            transform.position,
+            0.4f, 
+            Vector2.down, 
+            0.62f, 
+            LayerMask.GetMask("Ground")
+        );
+
+        //left wall detection
+        RaycastHit2D left_wall = Physics2D.BoxCast(
+            transform.position,
+            new Vector2(0.5f, 1.9f),
+            0f,
+            Vector2.left,
+            0.27f,
+            LayerMask.GetMask("Ground")
+        );
+
+        //right wall detection
+        RaycastHit2D right_wall = Physics2D.BoxCast(
+            transform.position,
+            new Vector2(0.5f, 1.9f),
+            0f,
+            Vector2.right,
+            0.27f,
+            LayerMask.GetMask("Ground")
+        );
 
         //placeholder sprint
         if(Input.GetKey(KeyCode.LeftShift)) {
             speed_mod = original_speed_mod * dash_multiplier;
+
         } else {
             speed_mod = original_speed_mod;
         }
 
         //movement
         movement.y = 0f;
-        Debug.DrawRay(transform.position, Vector2.down, Color.green);
         if(ground.collider != null) {
             movement.x = Input.GetAxisRaw("Horizontal") * speed_mod;
         } else {
+
             //air drift
-            movement.x += Input.GetAxisRaw("Horizontal") * Time.deltaTime * 5 * speed_mod;
-            movement.x = Mathf.Clamp(movement.x, -speed_mod, speed_mod);
+            if((movement.x <= original_speed_mod && Input.GetAxisRaw("Horizontal") == 1) 
+                || (movement.x >= -original_speed_mod && Input.GetAxisRaw("Horizontal") == -1))
+            {
+                movement.x += Input.GetAxisRaw("Horizontal") * Time.deltaTime * 5 * speed_mod;
+            }
+
             if(Input.GetAxisRaw("Horizontal") == 0) {
+
                 if(movement.x > 0) {
                     movement.x -= Time.deltaTime * 3 * speed_mod;
+
                 } else if(movement.x < 0) {
                     movement.x += Time.deltaTime * 3 * speed_mod;
                 }
@@ -71,6 +104,7 @@ public class PlayerMovement : MonoBehaviour
         if(left_wall.collider != null && movement.x < 0) {
             Debug.Log("Next to left wall");
             movement.x = 0f;
+
         } else if(right_wall.collider != null && movement.x > 0) {
             Debug.Log("Next to right wall");
             movement.x = 0f;
@@ -80,6 +114,7 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetButtonDown("Jump")) {
             if(ground.collider != null) {
                 velocity = jump_height;
+
             } else if(double_jump) {
                 velocity = jump_height;
                 double_jump = false;
@@ -94,7 +129,13 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //ceiling detection
-        RaycastHit2D ceiling = Physics2D.CircleCast(transform.position, 0.4f, Vector2.up, 0.62f, LayerMask.GetMask("Ground"));
+        RaycastHit2D ceiling = Physics2D.CircleCast(
+            transform.position, 
+            0.4f, 
+            Vector2.up, 
+            0.62f, 
+            LayerMask.GetMask("Ground")
+        );
         if(ceiling.collider != null) {
             velocity = 0f;
         }
@@ -109,14 +150,24 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate() {
         //gravity
-        RaycastHit2D ground = Physics2D.CircleCast(transform.position, 0.4f, Vector2.down, 0.62f, LayerMask.GetMask("Ground"));
+        RaycastHit2D ground = Physics2D.CircleCast(
+            transform.position, 
+            0.4f, 
+            Vector2.down, 
+            0.62f, 
+            LayerMask.GetMask("Ground")
+        );
+
         if(ground.collider == null) {
             velocity -= gravity_mod * Time.fixedDeltaTime;
+
         } else {
             velocity = Mathf.Clamp(velocity, 0, Mathf.Infinity);
             double_jump = true;
         }
+
         velocity = Mathf.Clamp(velocity, -fall_speed_limit, Mathf.Infinity);
+        
         //movement
         movement.y += velocity;
         rb.MovePosition((Vector2)rb.position + new Vector2(movement.x * Time.fixedDeltaTime, movement.y));
